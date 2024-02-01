557a9fe4457c983c9e222c27ea7c59b9e3372463
Matěj Goněc <matej.gonec@seznam.cz>
2024-02-01 09:04:58 +0100

    1.2.2024 Commit #1
    final version

--- ExcelToText.py
+++ ExcelToText.py
@@ -1,11 +1,31 @@
 import pandas as pd
+import xlwings as xw
+import os
 
-data = pd.read_excel("import_gen_v2.xlsm", "ImportKomplet")
 
-toPrint = data.test1.array
-impfile = open("test.imp", "w")
+for i in range(48):
+    if i < 9:
+        machine = 'N0{}'.format(i+1)
+    else:
+        machine = 'N{}'.format(i+1)
 
-for row in toPrint:
-    impfile.write("{} \n".format(row))
+    wb = xw.Book('testInput.xlsm')
+    ws = wb.sheets['Seznam']
+    ws['C2'].value = machine
+    wb.save('temp.xlsm')
+    wb.close()
 
-impfile.close()
+    data = pd.read_excel('temp.xlsm', 'ImportKomplet', header = None)
+
+    toPrint = data[0]
+
+    impfile = open('TEST/{}'.format(machine), 'w')
+
+    for row in toPrint:
+        if type(row) == float:
+            impfile.write('\n')
+        else:
+            impfile.write('{} \n'.format(row))
+    impfile.close()
+
+os.remove('test.xlsm')
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ExcelToImp
-Python script to export excel data to text file 
+Python script opens excel file, edits specific cell at specified sheet then exports updated value of another sheet into text file.
+
+Using pandas and xlwings libraries
